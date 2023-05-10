2279df1e45f759fd3fa9d1954fa6735647b0f227
Jehwan Yoo <yoojehwan1875@gmail.com>
2023-05-10 18:47:27 +0900

    Folder 구조 매핑

--- src/routes/Editor.tsx
+++ src/routes/Editor.tsx
@@ -4,10 +4,13 @@ import type ReactQuill from 'react-quill'
 import { createDocument, joinRoom } from '../lib/connect'
 import { QuillBinding } from 'y-quill'
 import type { WebrtcProvider } from 'y-webrtc'
+import { useLocation } from 'react-router-dom'
+import { FormControl, InputLabel, OutlinedInput, Tooltip } from '@mui/material'
+import { CloudOff, CloudQueue } from '@mui/icons-material'
 
 export function Editor() {
   const [provider, setProvider] = useState<WebrtcProvider | null>(null)
-  const [connected, setConnected] = useState(false)
+  const location = useLocation()
   const component = useRef<ReactQuill>(null)
 
   useEffect(() => {
@@ -29,18 +32,34 @@ export function Editor() {
     }
   }, [])
 
-  useEffect(() => {
-    setConnected(!!provider?.connected)
-  }, [provider?.connected])
-
   return (
     <section>
-      <header>
-        <h1 className="text-4xl">Editor</h1>
-      </header>
       <section>
-        {/*<div>connected: {connected ? '✅' : '❌'}</div>*/}
-        <QuillEditor className="my-8" quill={component} />
+        <section className="px-1">
+          {provider?.connected ? (
+            <Tooltip title="connected">
+              <CloudQueue color="success" />
+            </Tooltip>
+          ) : (
+            <Tooltip title="disconnected">
+              <CloudOff color="secondary" />
+            </Tooltip>
+          )}
+        </section>
+        <section className="my-4">
+          <FormControl fullWidth>
+            <InputLabel htmlFor="outlined-adornment-amount">
+              파일 이름
+            </InputLabel>
+            <OutlinedInput
+              id="outlined-adornment-amount"
+              label="Amount"
+              value={location.state.filename}
+            />
+          </FormControl>
+        </section>
+
+        <QuillEditor className="my-4" quill={component} />
       </section>
     </section>
   )
--- src/routes/Finder.tsx
+++ src/routes/Finder.tsx
@@ -1,20 +1,95 @@
 import React from 'react'
-import { TreeItem, TreeView } from '@mui/lab'
-import { Article, Folder, FolderOpen } from '@mui/icons-material'
+import { TreeView } from '@mui/lab'
+import { useNavigate } from 'react-router-dom'
+import merge from 'deepmerge'
+import { uuidv4 } from 'lib0/random'
 
-const json = [{ name: 'hello' }, { name: 'world' }]
+interface Item {
+  id: string
+  name: string
+}
+
+const items: Item[] = [
+  {
+    id: uuidv4(),
+    name: '부동산 리포트/11월/report-1',
+  },
+  {
+    id: uuidv4(),
+    name: '부동산 리포트/11월/report-2',
+  },
+  {
+    id: uuidv4(),
+    name: '부동산 리포트/12월/report',
+  },
+]
 
 export function Finder() {
+  const navigate = useNavigate()
+  let root: Record<string, object> = {}
+
+  function parseDepth(items: Item[]) {
+    for (const item of items) {
+      const tree: Record<string, object> = {}
+      const parsed = item.name.split('/')
+      let prevDirectory: string | undefined
+
+      for (const directory of parsed.slice(0, -1)) {
+        if (prevDirectory) {
+          tree[prevDirectory] = merge(tree[prevDirectory], { [directory]: {} })
+        } else {
+          tree[directory] = {}
+        }
+        prevDirectory = directory
+      }
+
+      if (prevDirectory) {
+        tree[prevDirectory] = {
+          [parsed.at(-1) as string]: { id: item.id },
+        }
+      }
+
+      root = merge(root, tree)
+    }
+  }
+
+  parseDepth(items)
+
+  console.log(root)
+
   return (
     <TreeView>
-      <TreeItem
-        nodeId="1"
-        label="folder"
-        collapseIcon={<FolderOpen />}
-        expandIcon={<Folder />}
-      >
-        <TreeItem nodeId="2" label="file" icon={<Article />} />
-      </TreeItem>
+      {Object.entries(root).map((item) => {
+        if (item) {
+          return (
+            <></>
+            // <TreeItem
+            //   key={item}
+            //   nodeId={item}
+            //   label="sample"
+            //   icon={<Article />}
+            //   onClick={() =>
+            //     navigate('/editor', { state: { filename: 'sample' } })
+            //   }
+            // />
+          )
+        } else {
+          return <p key={item}>good</p>
+        }
+      })}
+      {/*<TreeItem*/}
+      {/*  nodeId="1"*/}
+      {/*  label="folder"*/}
+      {/*  collapseIcon={<FolderOpen />}*/}
+      {/*  expandIcon={<Folder />}*/}
+      {/*>*/}
+      {/*  <TreeItem*/}
+      {/*    nodeId="2"*/}
+      {/*    label="sample"*/}
+      {/*    icon={<Article />}*/}
+      {/*    onClick={() => navigate('/editor', { state: { filename: 'sample' } })}*/}
+      {/*  />*/}
+      {/*</TreeItem>*/}
     </TreeView>
   )
 }
