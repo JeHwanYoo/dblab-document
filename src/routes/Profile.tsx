75d49f2726aa36ccc7031da12738f6d0c596539c
Jehwan Yoo <yoojehwan1875@gmail.com>
2023-05-11 15:17:11 +0900

    프로필 수정 기틀

--- src/routes/Profile.tsx
+++ src/routes/Profile.tsx
@@ -1,14 +1,55 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import Button from '@mui/material/Button'
 import { AmplifyUser } from '@aws-amplify/ui'
+import { FormControl, InputLabel, OutlinedInput } from '@mui/material'
 
 interface Props {
   user?: AmplifyUser
 }
 
-export function Profile(props: Props) {
+export function Profile({ user }: Props) {
+  const initNickname = user?.attributes?.nickname
+
+  const [nickname, setNickname] = useState(user?.attributes?.nickname)
+  const [dirty, setDirty] = useState(false)
+
+  const handleNameChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNickname(event.target.value)
+  }
+
+  const handleSave = () => {
+    // Save changes to backend API
+    setDirty(false)
+  }
+
+  useEffect(() => {
+    setDirty(initNickname !== nickname)
+  }, [nickname])
+
   return (
-    <header>
-      <h1 className="text-4xl">Profile</h1>
-    </header>
+    <>
+      <section className="mb-8">
+        <FormControl fullWidth>
+          <InputLabel htmlFor="outlined-nickname">닉네임</InputLabel>
+          <OutlinedInput
+            id="outlined-nickname"
+            label="path"
+            value={nickname}
+            onChange={handleNameChange}
+          />
+        </FormControl>
+      </section>
+      <section>
+        <Button
+          type="submit"
+          sx={{ width: 300, height: 50 }}
+          variant="contained"
+          disabled={!dirty}
+          onClick={handleSave}
+        >
+          Save
+        </Button>
+      </section>
+    </>
   )
 }
